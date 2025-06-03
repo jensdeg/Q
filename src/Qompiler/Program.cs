cf19d7a7b742fbe4033ab1e7cfbeccfd5f1eca1e
jensdeg <jensdegraaf88@gmail.com>
2025-06-03 22:45:11 +0200

    filereader

--- src/Qompiler/Program.cs
+++ src/Qompiler/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello world");
+﻿using Qompiler.Tools;
+
+if (args.Length == 0) return;
+var fileName = args[0];
+var file = FileFinder.Find(fileName);
+
+
+using var reader = new StreamReader(file);
+var content = reader.ReadToEnd();
+
+Console.WriteLine($"Compiling {fileName}");
+
---
+++ src/Qompiler/Tools/FileFinder.cs
@@ -0,0 +1,32 @@
+﻿namespace Qompiler.Tools
+{
+    public class FileFinder
+    {
+        /// <summary>
+        /// This is very specific to the Qompiler project structure.
+        /// made so it works with both the IDE and the command line.
+        /// very brittle, but it works for now.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static string Find(string fileName)
+        {
+            var directoryPath = Environment.CurrentDirectory;
+            if (directoryPath.Contains("bin"))
+            {
+                directoryPath = directoryPath.Substring(0, directoryPath.IndexOf("bin", StringComparison.Ordinal));
+            }
+            if (directoryPath.Contains('Q'))
+            {
+                directoryPath = directoryPath.Substring(0, directoryPath.IndexOf("src", StringComparison.Ordinal));
+                var fullPath = directoryPath + @"examples\" + fileName;
+                if(File.Exists(fullPath))
+                {
+                    return fullPath;
+                }
+                return new FileNotFoundException($"File {fullPath} not found").Message;
+            }
+            throw new NotImplementedException("Not supported Yet");
+        }
+    }
+}
