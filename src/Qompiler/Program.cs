15a154b5af85c5295fbdb8c4fa89638a2a47603a
jensdeg <jensdegraaf88@gmail.com>
2026-01-30 20:30:24 +0100

    Nuke

--- src/Qompiler/Program.cs
+++ src/Qompiler/Program.cs
@@ -1,20 +1,5 @@
-﻿using Qompiler;
-using Qompiler.CodeGen;
-using Qompiler.Tools;
-
-if (args.Length == 0) return;
+﻿if (args.Length == 0) return;
 var fileContent = File.ReadAllText(args[0]);
 var filename = args[0].Split('.')[0];
 
 Console.WriteLine($"Compiling '{args[0]}'{Environment.NewLine}");
-
-var tokens = Lexer.Tokenize(fileContent);
-var operations = Parser.Parse(tokens);
-var code = Nasm.Generate(operations);
-
-FileManager.WriteAsm(filename, code);
-
-Console.WriteLine("Finished compiling");
-
-Console.WriteLine("Running Build...");
-Runner.RunAssembly(filename);
